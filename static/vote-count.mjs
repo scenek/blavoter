6ab8b189f1ea6943e30d76c2c916b0bb350ee27c
Stepan Cenek <scenek@gmail.com>
2026-07-27 11:38:26 +0200

    fix: vote count

--- static/vote-count.mjs
+++ static/vote-count.mjs
@@ -1,5 +1,8 @@
 export function formatVoteCount(count) {
     if (count === 0) return "0 hlasů";
     if (count === 1) return "1 hlas";
-    return `${count} hlasy`;
+    if (count === 2) return "2 hlasy";
+    if (count === 3) return "3 hlasy";
+    if (count === 4) return "4 hlasy";
+    return `${count} hlasů`;
 }
